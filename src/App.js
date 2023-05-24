b8be24fa98aefa8ac6b72f4823254a373d29e9b3
Abhishek Tyagi <abhishektyagi99831@gmail.com>
2023-05-24 14:40:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <News />
+      </div>
+    );
+  }
+}
